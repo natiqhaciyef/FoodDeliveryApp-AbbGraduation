84472744b5844c4de8bb3b74dcfa2742ff4e9168
Natiq <natiq00h2272@gmail.com>
2022-11-26 19:28:11 +0400

    Architecture response

--- app/src/main/java/com/natiqhaciyef/fooddeliveryapp_abbgraduation/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/natiqhaciyef/fooddeliveryapp_abbgraduation/ui/register/RegisterFragment.kt
@@ -21,6 +21,6 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        println()
     }
 }
