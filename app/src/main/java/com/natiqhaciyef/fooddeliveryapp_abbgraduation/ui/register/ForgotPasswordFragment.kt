e566945c562bb33097db2306d387e4e11ca6df68
Natiq <natiq00h2272@gmail.com>
2022-11-27 15:39:37 +0400

    Forgot Password UI ready

--- app/src/main/java/com/natiqhaciyef/fooddeliveryapp_abbgraduation/ui/register/ForgotPasswordFragment.kt
+++ app/src/main/java/com/natiqhaciyef/fooddeliveryapp_abbgraduation/ui/register/ForgotPasswordFragment.kt
@@ -6,15 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.natiqhaciyef.fooddeliveryapp_abbgraduation.R
+import com.natiqhaciyef.fooddeliveryapp_abbgraduation.databinding.FragmentForgotPasswordBinding
 
 
 class ForgotPasswordFragment : Fragment() {
-
+    private lateinit var binding: FragmentForgotPasswordBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
+        binding = FragmentForgotPasswordBinding.inflate(inflater, container, false)
 
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
