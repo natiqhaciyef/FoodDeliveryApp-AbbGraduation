77cfdc84470fa5818f18dd0344950f0747dd2d77
Natiq <natiq00h2272@gmail.com>
2022-11-28 19:37:03 +0400

    Cart item click ready

--- app/src/main/java/com/natiqhaciyef/fooddeliveryapp_abbgraduation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/natiqhaciyef/fooddeliveryapp_abbgraduation/ui/home/HomeFragment.kt
@@ -5,19 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.natiqhaciyef.fooddeliveryapp_abbgraduation.R
+import com.natiqhaciyef.fooddeliveryapp_abbgraduation.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.homeFragment = this
 
     }
 }
