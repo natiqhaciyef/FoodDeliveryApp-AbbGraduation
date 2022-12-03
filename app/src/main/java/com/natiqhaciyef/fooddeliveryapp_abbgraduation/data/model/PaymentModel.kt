0e50553a3d80d2c6a01750eed628fc68d233e32f
Natiq <natiq00h2272@gmail.com>
2022-12-03 13:33:49 +0400

    Payment model and initial payment structure ready

---
+++ app/src/main/java/com/natiqhaciyef/fooddeliveryapp_abbgraduation/data/model/PaymentModel.kt
@@ -0,0 +1,6 @@
+package com.natiqhaciyef.fooddeliveryapp_abbgraduation.data.model
+
+data class PaymentModel(
+    var name: String,
+    var totalPrice: Int
+)
---
+++ app/src/main/java/com/natiqhaciyef/fooddeliveryapp_abbgraduation/util/PaymentList.kt
@@ -0,0 +1,16 @@
+package com.natiqhaciyef.fooddeliveryapp_abbgraduation.util
+
+import com.natiqhaciyef.fooddeliveryapp_abbgraduation.data.model.PaymentModel
+
+object PaymentList {
+    val list = mutableListOf<PaymentModel>(
+        PaymentModel("Meatball", 32),
+        PaymentModel("Baklava", 75),
+        PaymentModel("Grilled Salmon", 60),
+        PaymentModel("Ayran", 2),
+        PaymentModel("Coffee",6),
+        PaymentModel("Pizza",36),
+        PaymentModel("Sutlac",12),
+        PaymentModel("Pizza",18)
+    )
+}
