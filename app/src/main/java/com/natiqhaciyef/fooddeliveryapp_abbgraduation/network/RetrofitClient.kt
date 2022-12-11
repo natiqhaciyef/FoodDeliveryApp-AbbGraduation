6dc81350f59f48319dab0f34da0d663109e83ead
Natiq <natiq00h2272@gmail.com>
2022-12-11 12:09:05 +0400

    Design changes 3

--- app/src/main/java/com/natiqhaciyef/fooddeliveryapp_abbgraduation/network/RetrofitClient.kt
+++ app/src/main/java/com/natiqhaciyef/fooddeliveryapp_abbgraduation/network/RetrofitClient.kt
@@ -6,7 +6,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RetrofitClient {
     companion object {
         fun getClient(baseUrl: String): Retrofit {
-            return Retrofit.Builder().baseUrl(baseUrl)
+            return Retrofit.Builder()
+                .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         }
